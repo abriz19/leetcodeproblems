79200bfdf7aaea33390d6e8eef2eeff0401e6fe7
Abreham Dessu <abreham.dessu@gmail.com>
2025-09-23 22:09:55 +0300

    [650] [Time Beats: 100.00%] [Memory Beats: 78.15%] - LeetPush

---
+++ 2-Keys-Keyboard.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        if n == 1:
+            return 0
+        
+        ans, d = 0, 2
+        while n > 1:
+            while n % d == 0:
+                ans += d
+                n //= d
+            d += 1
+        return ans
